3bf8b3c5f1c726037d031b09ee5c6956134ec2ee
alex <alexander.schnedler@gmail.com>
2017-01-27 15:09:29 -0600

    game prototype working

--- HangMan.rb
+++ HangMan.rb
@@ -9,28 +9,29 @@ class HangMan
     @word.length.times {@board += '-'}
   end
 
-  def correct_guess(guess)
-    if @word.index(guess)
+  def correct_guess(guess, word)
+    if word.index(guess)
       return true
     end
     return false
   end
 
-  def victory()
-    if @board.index(@word)
+  def victory(word, board)
+    if board.index(word)
       return true
     end
     return false
   end
 
-  def modify_board(guess)
+  def modify_board(guess, word, board)
     i = 0
-    @word.each_char do |e|
+    word.each_char do |e|
       if e == guess
-        @board[i] = e
+        board[i] = e
       end
       i += 1
     end
+    return board
   end
 
   def board
--- hang_man_web.rb
+++ hang_man_web.rb
@@ -2,38 +2,52 @@ require_relative 'HangMan.rb'
 require 'sinatra'
 require "bundler/setup"
 
-before do
-  @game = HangMan.new("tree")
-  @game.generate_board
+configure do
+  enable :sessions
+  set :session_secret, "secret"
 end
 
 get '/' do
-  @board = @game.board
-  @message = 'you have turns left'
+  @session = session
+  word = "tree"
+  game = HangMan.new(word)
+  game.generate_board
+  session["counter"] = 6
+  session["word"] = word
+  session["board"] = game.board
+  @board = session["board"]
+  @message = 'you have 6 turns left'
   erb :index
 end
 
 post '/guess' do
-  #@game = HangMan.new("tree")
-  #@game.generate_board
+  board = session["board"]
+  #puts ">>>>>>>>>>>>>> #{board}<<<<<<<<<<<<<<<<<<<<<<<<"
+  word = session["word"]
+  counter = session["counter"]
+
   guess = params['guess']
-  if @game.correct_guess(guess)
-    @game.modify_board(guess)
-    if @game.victory
-      @game.print_board
-      @message = "victory!"
-      break
+
+  game = HangMan.new(word)
+  game.generate_board
+  if game.correct_guess(guess, word)
+    board = game.modify_board(guess, word, board)
+    message = "you have #{counter} guesses left"
+    if game.victory(word, board)
+      board = board
+      message = "victory!"
     end
   else
-    @message =  "you have guesses left"
     counter -= 1
+    message =  "you have #{counter} guesses left"
+
+    session["counter"] = counter
     if counter == 0
-      @message =  "you lose!!!"
-      break
+      message =  "you lose!!!"
+      puts ">>>>>>>>>>>>>> #{counter}<<<<<<<<<<<<<<<<<<<<<<<<"
     end
   end
-  @board = @game.board
-
-  @message = 'you have turns left'
+  @board = board
+  @message = message
   erb :index
 end
